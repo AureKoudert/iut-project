574f986ba4032dd22e9486df1edefd09d33fd264
Devin Ivy <devin@bigroomstudios.com>
2021-04-11 23:24:33 -0400

    first commit

--- server/manifest.js
+++ server/manifest.js
@@ -3,6 +3,7 @@
 const Dotenv = require('dotenv');
 const Confidence = require('@hapipal/confidence');
 const Toys = require('@hapipal/toys');
+const Schwifty = require('@hapipal/schwifty');
 
 // Pull .env into process.env
 Dotenv.config({ path: `${__dirname}/.env` });
@@ -35,6 +36,29 @@ module.exports = new Confidence.Store({
             },
             {
                 plugin: './plugins/swagger'
+            },
+            {
+                plugin  : '@hapipal/schwifty',
+                options : {
+                    $filter    : 'NODE_ENV',
+                    $default   : {},
+                    $base      : {
+                        migrateOnStart : true,
+                        knex           : {
+                            client     : 'mysql',
+                            connection : {
+                                host     : process.env.DB_HOST || 'localhost',
+                                user     : process.env.DB_USER || 'root',
+                                password : process.env.DB_PASSWORD || 'hapi',
+                                database : process.env.DB_DATABASE || 'user',
+                                port     : process.env.DB_PORT || 3307
+                            }
+                        }
+                    },
+                    production : {
+                        migrateOnStart : false
+                    }
+                }
             },
             {
                 plugin: {
